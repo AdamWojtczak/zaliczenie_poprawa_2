2454e80657607ef21da69bb485b4fab1edfcf534
wojtczak.adam <215922@edu.p.lodz.pl>
2020-09-15 17:50:51 +0200

    Call order test
    Signed-off-by: wojtczak.adam <215922@edu.p.lodz.pl>

--- testsReactor/src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
@@ -3,10 +3,13 @@ package edu.iis.mto.testreactor.doser;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import edu.iis.mto.testreactor.doser.infuser.Infuser;
+import edu.iis.mto.testreactor.doser.infuser.InfuserException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.concurrent.TimeUnit;
@@ -21,6 +24,7 @@ class MedicineDoserTest {
 	@Mock
 	private Infuser infuser;
 	private MedicineDoser medicineDoser;
+	private Receipe recipe;
 
 	@Test
 	void itCompiles() {
@@ -30,15 +34,30 @@ class MedicineDoserTest {
 	@BeforeEach
 	void setUp() {
 		medicineDoser = new MedicineDoser(infuser, dosageLog, clock);
+		recipe = Receipe.of(Medicine.of("APAP"),
+				Dose.of(Capacity.of(1, CapacityUnit.MILILITER),
+						Period.of(1, TimeUnit.DAYS)), 1);
+		medicineDoser.add(MedicinePackage.of(Medicine.of("APAP"), Capacity.of(100, CapacityUnit.MILILITER)));
 	}
 
 	@Test
 	void dosingTest_shouldResultInSucces() {
-		medicineDoser.add(MedicinePackage.of(Medicine.of("APAP"), Capacity.of(100, CapacityUnit.MILILITER)));
-		Receipe recipe = Receipe.of(Medicine.of("APAP"),
-                          Dose.of(Capacity.of(10, CapacityUnit.MILILITER),
-                          Period.of(1, TimeUnit.DAYS)), 10);
 		DosingResult dosingResult = medicineDoser.dose(recipe);
 		assertEquals(dosingResult, DosingResult.SUCCESS);
 	}
+
+	@Test
+	void callOrderTest() throws InfuserException {
+		medicineDoser.dose(recipe);
+		InOrder order = Mockito.inOrder(clock, dosageLog, infuser);
+		order.verify(dosageLog).logStart();
+		order.verify(dosageLog).logStartDose(recipe.getMedicine(), recipe.getDose());
+		order.verify(infuser).dispense( Mockito.any(), Mockito.any());
+		order.verify(dosageLog).logEndDose(recipe.getMedicine(), recipe.getDose());
+		order.verify(clock).wait(recipe.getDose().getPeriod());
+		order.verify(dosageLog).logEnd();
+	}
+
+
+
 }
